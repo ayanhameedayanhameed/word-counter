092f9fc3cc55c6fdde594a4ee698f08aeccee220
Muhammad Ayan <158440887+ayanhameedayanhameed@users.noreply.github.com>
2024-04-18 14:31:59 +0500

    adding chalk

--- index.ts
+++ index.ts
@@ -13,12 +13,12 @@ const answers: {
 } = await inquirer.prompt([{
     name: "sentence",
     type: "input",
-    message: "Enter your sentence to count the word: "
+    message: chalk.yellow("Enter your sentence to count the word: ")
 }])
 
  const words = answers.sentence.trim().split(" ")
 
  console.log(words)
 
- console.log(`Your sentence word count is ${words.length}`);
+ console.log(chalk.green(`Your sentence word count is ${words.length}`));
  
